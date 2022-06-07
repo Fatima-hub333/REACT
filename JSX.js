605ee4469668a0f5b50b9fb12156c5075b1f1d8a
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-07 14:47:20 +0500

    Practice JSX Examples

---
+++ JSX.js
@@ -0,0 +1,37 @@
+// const page = (
+//    <div>
+//       <h1 className="header">This is JSX</h1>
+//       <p>This is a Paragraph</p>
+//     </div>
+// )
+
+// ReactDOM.render(page,
+//     document.getElementById("root")
+// )
+
+/* 
+Challenge: 
+
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
+
+const navBar = (
+  <nav>
+    <h1> Heaven Technologies </h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+)
+
+ReactDOM.render(
+  navBar, document.getElementById("root")
+)
---
+++ declarative.js
@@ -0,0 +1,19 @@
+ReactDOM.render(
+  <h1>My Awsome Website in React</h1>,
+  document.getElementById("root")
+)
+
+ReactDOM.render(
+  <h4>Reasons I Love React</h4>,
+  document.getElementById("content")
+)
+
+ReactDOM.render(
+  <ul>
+    <li>It's Composable</li>
+    <li>It's declarative</li>
+    <li>It's hireable Skill</li>
+    <li>It's Actively Maintained by Skilled People</li>
+  </ul>,
+  document.getElementById("reasons")
+)
