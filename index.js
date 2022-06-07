99b13d6adf8aeed365ce668e946e4a452294704b
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-07 14:14:29 +0500

    Practice "Why React declarative?" with examples

--- index.js
+++ index.js
@@ -1,52 +1,19 @@
-function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled" href="#">Disabled</a>
-                </li>
-                </ul>
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-        </nav>
-    )
-}
-
-function MainContent() {
-  return (
-    <h2>I am learning React</h2>
-  )
-}
+ReactDOM.render(
+  <h1>My Awsome Website in React</h1>,
+  document.getElementById("root")
+)
 
 ReactDOM.render(
-  <div>
-    <Navbar />
-    <MainContent />
-  </div>,
-  document.getElementById("root"))
+  <h4>Reasons I Love React</h4>,
+  document.getElementById("content")
+)
+
+ReactDOM.render(
+  <ul>
+    <li>It's Composable</li>
+    <li>It's declarative</li>
+    <li>It's hireable Skill</li>
+    <li>It's Actively Maintained by Skilled People</li>
+  </ul>,
+  document.getElementById("reasons")
+)
