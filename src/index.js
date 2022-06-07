e36f911c81384971aba52b9e4c64467dadf598f2
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-07 16:03:47 +0500

    Update Index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-const navbar = (
-  <nav>
-    <h1>Bob's Bistro</h1>
-    <ul>
-      <li>Menu</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
-  </nav>
+const page = (
+  <div>
+    <h1>My website in React</h1>
+    <h3>Reasons I love React</h3>
+    <ol>
+      <li>It's composable</li>
+      <li>It's declarative</li>
+      <li>It's declarativeIt's a hireable skill</li>
+      <li>It's actively maintained by skilled people</li>
+    </ol>
+  </div>
 )
 
-ReactDOM.render(navbar, document.getElementById("root"))
+ReactDOM.render(page, document.getElementById("root"))
