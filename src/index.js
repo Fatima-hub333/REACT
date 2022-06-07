9812b506a962923adf9b3b8fc586769027a29155
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-07 22:28:37 +0500

    Practice functions Examples

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-const page = (
+function temporaryName() {
+  return (
     <div>
-        <img src="./react-logo.png" width="40px" alt="Logo"/>
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-)
+      <img src="./react-logo.png" width="40px" alt="Logo"/>
+      <h1>Fun Facts About React </h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
 
-ReactDOM.render(page, document.getElementById("root"))
+    </div>
+  )
+}
+ReactDOM.render(temporaryName(), document.getElementById("root"))
