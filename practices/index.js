e17b56d5fc18ed14aa147d0cd52194a7fe273ca0
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-07 16:23:45 +0500

    Update files

---
+++ practices/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const page = (
+  <div>
+    <h1>My website in React</h1>
+    <h3>Reasons I love React</h3>
+    <ol>
+      <li>It's composable</li>
+      <li>It's declarative</li>
+      <li>It's declarativeIt's a hireable skill</li>
+      <li>It's actively maintained by skilled people</li>
+    </ol>
+  </div>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 const page = (
-  <div>
-    <h1>My website in React</h1>
-    <h3>Reasons I love React</h3>
-    <ol>
-      <li>It's composable</li>
-      <li>It's declarative</li>
-      <li>It's declarativeIt's a hireable skill</li>
-      <li>It's actively maintained by skilled people</li>
-    </ol>
-  </div>
+    <div>
+        <img src="images/react-logo.png" width="40px" alt="Logo"/>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
 )
 
 ReactDOM.render(page, document.getElementById("root"))
