3e70fb0e33432a97e8b5535bd2ee906fceec5027
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-09 14:42:58 +0500

    practice parent/child component

--- src/challenges/custom-pagecomponent.js
+++ src/challenges/custom-pagecomponent.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-const customPage = (
-  <div>
-    <h1>I want to Learn React because of the following reasons;</h1>
-    <ol>
-      <li>React use Composable Coding</li>
-      <li>React is Fast & easy to understand</li>
-      <li>React is a hireable skill</li>
-    </ol>
+function Page() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <img src={require('./react-logo.png')} width="40px" alt="Logo"/>
+        </nav>
+      </header>
+      <h1>Reasons I'm Excited to Learn React</h1>
+      <ol>
+        <li>It's a popular library, so I'll be
+          able to fit in with the cool kids!
+        </li>
+        <li>
+          I'm more likely to get a job as a developer
+          if I know React
+        </li>
+      </ol>
+      <footer>
+        <small>@ 2022 Fatima Development. All rights reserved.</small>
+      </footer>
   </div>
-);
+  )
+ 
+};
 
-ReactDOM.render(customPage, document.getElementById("root"))
+ReactDOM.render(<Page />, document.getElementById("root"))
