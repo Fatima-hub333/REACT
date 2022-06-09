a140ba59fdfb32b8748e77cdea8edbcd7200cee2
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-09 16:03:48 +0500

    Create linters.yml

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Logo from './react-logo.png'
 
 export default function Header() {
   return (
       <header>
           <nav className="nav">
-        <img className ="logo" src={require("./react-logo.png")} alt="Logo"/>
+        <img className ="logo" src={Logo} alt="Logo"/>
               <ul className="nav-items">
                   <li>Pricing</li>
                   <li>About</li>
